585a82f1e5ae4e39da2b3477fc2ae56c30bb36c5
Eric Masser <eric@massers.se>
2022-04-26 15:31:01 +0200

    La till lite filer och fixade basfunktioner i servern.

--- server.py
+++ server.py
@@ -1,24 +1,29 @@
 from random import randint
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from pathlib import Path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST","GET"])
 def multiply():
     if request.method == "GET":
-        # Show the default
         x = randint(1, 100)
         y = randint(1, 100)
-        return render_template("index.html", integer=str(x * y))
+        return {"randint": x * y,
+                "start processing": "make a POST request with an image of a suspected deer to /",
+                "list images": "/list-images"}
     elif request.method == "POST":
-        # An image was uploaded
-        return {"filename": "exampleName.jpg", "status": "uploaded"}
+        f = request.files["the_file"]
+        f.save(f"/var/www/viltkamera.lugash.se/static/images/{secure_filename(f.filename)}")
+        return {"filename": f.filename, "status": "uploaded"}
 
 @app.route("/list-images")
 def listImages():
-    imageHandle = Path("images")
-    return render_template("imageList.html", imageList=imageHandle.iterdir())
+    imageHandle = Path("static/images")
+    tmpList = ["images/" + image.name for image in imageHandle.iterdir()]
+    print(tmpList[0])
+    return render_template("imageList.html", imageList=tmpList)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
